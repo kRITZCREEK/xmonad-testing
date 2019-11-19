0e552896a8c24e08740294974d9c9143cef8d6db
Christoph <christoph.hegemann1337@gmail.com>
2019-11-19 12:27:39 +0100

    starts the nm-applet on startup

--- config.hs
+++ config.hs
@@ -264,6 +264,7 @@ myAddSpaces len str = sstr ++ replicate (len - length sstr) ' '
 myStartupHook = do
   setWMName "LG3D"
   spawn "$HOME/.xmonad/polybar.sh"
+  spawn "nm-applet" -- Starts the network manager tray icon
   -- spawn "dropbox"
 
 -----------------------------------------------------------------------------}}}
